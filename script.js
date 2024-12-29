d95e35e5ebc16e7f59dda9c15a98d94852ec3c24
sammy-the-great <rmg42016@gmail.com>
2024-12-29 17:59:35 +0100

    classes in java script

---
+++ script.js
@@ -0,0 +1,17 @@
+let student1={
+    name:"Ashton",
+    age:18,
+    hobbies:["parkour","movies"]
+}
+class student{
+    constructor(name,age,hobby){
+        this.name=name
+        this.age=age
+        this.hobby=hobby
+    }
+    }
+
+let student2=new student("paxton",16,"video games")
+let student3=new student("sammy",12,"video games")
+
+console.log("my name is",student2.name,"and I like ",student2.hobby)
